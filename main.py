8ed19549b34b6fcba7a05ebb696c04ffd8eb4e94
rafaelsilvausa <128944830+rafaelsilvausa@users.noreply.github.com>
2026-02-04 21:50:46 -0300

    Update main.py
    melhoras no codigo

--- main.py
+++ main.py
@@ -24,11 +24,14 @@ def main():
         total_pages = data["Paging"]["Offset"]
         for res in data["Results"]:
             sku = res["Identification"]["SKU"]
-            if sku == "HG 33088" :
+            if sku == "AC 25001" :
+                print("-"*80)
                 for i in res["Price"]:
                     preco = i["Price"]
-                    print(f" o preço de {sku} é {preco}") 
-
+                    print(f" o preço de {sku} é {preco}")
+                qd = res["Stock"]["Quantity"]
+                print(f"Sua quantidade em estoque é: {qd}")
+                print("-"*80)
     if pages < total_pages:
         pages+=100
     else:

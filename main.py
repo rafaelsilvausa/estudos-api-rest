54fbd45c16b8100f83c93ec3539bbd9e6b108190
rafaelsilvausa <128944830+rafaelsilvausa@users.noreply.github.com>
2026-02-04 21:38:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from dotenv import load_dotenv
+import sys
+import os
+
+load_dotenv()
+key = os.getenv("KEY")
+id = os.getenv("ID")
+def api (page):
+    url = f"https://api.alephcrm.com/v2/products?accountId={id}&api_key={key}"
+
+    payload = {}
+    headers = {
+    'Accept': 'application/json'
+    }
+
+    return requests.get(url, headers=headers, data=payload)
+
+def main():
+    pages = 0
+    dado = api(pages)
+    data = dado.json()
+    if dado.status_code == 200:
+        total_pages = data["Paging"]["Offset"]
+        for res in data["Results"]:
+            sku = res["Identification"]["SKU"]
+            if sku == "HG 33088" :
+                for i in res["Price"]:
+                    preco = i["Price"]
+                    print(f" o preço de {sku} é {preco}") 
+
+    if pages < total_pages:
+        pages+=100
+    else:
+        sys.exit()      
+                
+
+
+
+if __name__ == "__main__":
+    main()
